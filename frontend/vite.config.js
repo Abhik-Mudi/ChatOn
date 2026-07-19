11780d570ff67962eea9e89b656430e3f49ee0b0
Abhik-Mudi <abhikmudi6@gmail.com>
2026-07-19 18:14:32 +0530

    env loaded before build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000';
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api":{
-        target: apiUrl,
+export default defineConfig(({mode})=>{
+
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  const apiUrl = env.VITE_API_URL || 'http://localhost:5000';
+
+  return{
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api":{
+          target: apiUrl,
+        }
       }
     }
   }
